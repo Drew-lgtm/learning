b8a56e6e3a48286a0824ce228097a88014acf336
drew-lgtm <martinek.ondrej.cz@gmail.com>
2024-07-28 00:39:09 +0200

    tailwind update

--- next-app/app/components/ProductCard.tsx
+++ next-app/app/components/ProductCard.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import AddToCart from './AddToCart'
 
+
 const ProductCard = () => {
   return (
-    <div>
-        <AddToCart/>
+    <div className='p-0 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
+        <AddToCart />
     </div>
   )
 }
--- next-app/app/users/page.tsx
+++ next-app/app/users/page.tsx
@@ -1,12 +1,56 @@
-import React from 'react'
+"use client"; // This directive makes the component a Client Component
 
-const UsersPage = () => {
-  return (
-    <div>
-        <li>UsersPage</li>
-        <li><a href='/new'>NewUser</a></li>
-    </div>
-  );
+import React, { useState, useEffect } from 'react';
+
+// definition of shape of user objects
+interface User {
+    id: number;
+    name: string;
+    email: string; // Corrected typo: changed 'emai' to 'email'
 }
 
-export default UsersPage
+const UsersPage: React.FC = () => {
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            // returns a promise so we need to use 'await'. While using await, component needs to be async
+            // res = response
+            const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+                cache: 'no-store'
+                // revalidate every X s
+                // next: { revalidate: 10 }
+            });
+            // awaiting response to get the data + annotation of its type (User [])
+            const data: User[] = await res.json();
+            setUsers(data);
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <h1>Users</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => (
+                        <tr key={user.id}>
+                            <td>{user.name}</td> {/* Corrected from <th> to <td> and added {} */}
+                            <td>{user.email}</td> {/* Corrected from <th> to <td> and added {} */}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default UsersPage;
