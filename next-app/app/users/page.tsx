59d36a8777d1ee8cb2f4518080192f274435cc92
drew-lgtm <martinek.ondrej.cz@gmail.com>
2024-07-27 18:56:51 +0200

    basic structure

---
+++ next-app/app/users/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UsersPage = () => {
+  return (
+    <div>
+        <li>UsersPage</li>
+        <li><a href='/new'>NewUser</a></li>
+    </div>
+  );
+}
+
+export default UsersPage
